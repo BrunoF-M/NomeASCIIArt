4213a75bd1f983eecdb2528b5094e70194e48f84
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-11-13 20:31:48 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Figgle;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.ForegroundColor=ConsoleColor.DarkCyan;
+            Console.WriteLine("+-=-=AsCIIArt=-=-+");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            string nome;
+
+            Console.Write(" Digite uma qualquer coisa: ");
+            nome = Console.ReadLine();
+
+            string texto = FiggleFonts.Starwars.Render(nome);
+
+            Console.ForegroundColor=ConsoleColor.DarkGreen;                        
+            Console.WriteLine($" {texto}");
+            Console.ResetColor();
+
+            Console.Write("enter to finish...");
+            Console.ReadKey();
+        }
+    }
+}
